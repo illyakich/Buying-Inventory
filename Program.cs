0fc37936473cd9e0c3f497c04d6465f83aed8437
illyakich <speedfeed123@gmail.com>
2024-09-19 20:54:25 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("The following items are avaliable: \n 1 - Rope \r \n 2 - Torches \r \n 3 - Climbing Equipment \r \n 4 - Clean Water \r \n 5 - Machete \r \n 6 - Canoe \r \n 7 - Food Supplies \r");
+Console.Write("What number do you want to see the price of? ");
+string response;
+
+int choice = Convert.ToInt32(Console.ReadLine()); // i think they guy who made the book didnot declare that? im just a dumbass probably lol
+
+response = choice switch
+{ 
+    1 => "10 gold",
+    2 => "20 gold",
+    3 => "25 gold",
+    4 => "1 gold",
+    5 => "20 gold",
+    6 => "200 gold",
+    7 => "1 gold",
+    _ => "i dunno what that is"
+};
+
+Console.WriteLine(response);
